e783db7dd9079f73d3591de877510332e12b62e1
davirezo <fksdavi@gmail.com>
2026-08-19 21:26:11 -0300

    adicionando validaçoes

---
+++ api/src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes.agendamento;
+
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionamentoClinica {
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaDaClinica = dataConsulta.getHour()< 7;
+        var depoisDoEncerramentoDaClinica = dataConsulta.getHour() > 18;
+
+        if(domingo || antesDaAberturaDaClinica || depoisDoEncerramentoDaClinica){
+            throw new ValidationException("Consulta fora do horario de funcionamento da clinica");
+        }
+    }
+}
